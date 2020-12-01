87c25058c64312d8cf6d5adba65d0cb407b4add5
David Dinh <david@kpstudio.co.uk>
2020-12-01 14:43:37 +0000

    add comments

--- 1a-solution.js
+++ 1a-solution.js
@@ -1,5 +1,7 @@
 const fs = require("fs")
 let inputText = fs.readFileSync("./1a-input.txt", "utf-8")
 let input = inputText.split("\n").map(e => parseInt(e))
-let entry = input.filter(e => input.indexOf(2020 - e) !== -1)
-console.log(entry[0] * entry[1])
+
+// Rather than pairwise loops, we know that (2020 - e) will give us the value of the other key
+let elements = input.filter(e => input.indexOf(2020 - e) !== -1)
+console.log(elements[0] * elements[1])
--- 1b-solution.js
+++ 1b-solution.js
@@ -2,6 +2,8 @@ const fs = require("fs")
 let inputText = fs.readFileSync("./1a-input.txt", "utf-8")
 let input = inputText.split("\n").map(e => parseInt(e))
 
+// Extract the previous solution into a function so that we can re-apply it
+// after subtracting a first element
 let findTwoElementsThatSumTo = (sum, array) => array.filter(e => array.indexOf(sum - e) !== -1)
 let elements = input.filter((e,i) => findTwoElementsThatSumTo((2020 - e), input).length == 2)
 
