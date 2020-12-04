0a05b046ab5e849b5422f38399be5e7b944f06f4
David Dinh <david@kpstudio.co.uk>
2020-12-04 13:02:03 +0000

    add some comments and line breaks

--- 4b-solution.js
+++ 4b-solution.js
@@ -1,5 +1,6 @@
 const requiredFields = ["byr", "iyr", "eyr", "hgt", "hcl", "ecl", "pid"]
 const validEyeColors = ["amb", "blu", "brn", "gry", "grn", "hzl", "oth"]
+
 let validPassports = require("fs")
   .readFileSync("./4-input.txt", "utf-8")
   .split("\n\n")
@@ -12,14 +13,18 @@ let validPassports = require("fs")
     }, {})
   )
   .filter(e => {
+    // Check fields are present
     let hasRequiredFields =
       requiredFields.filter(field => e[field]).length == requiredFields.length
     if (!hasRequiredFields) return false
+
+    // Check fields are valid
     let byr = parseInt(e.byr)
     let iyr = parseInt(e.iyr)
     let eyr = parseInt(e.eyr)
     let hgt = parseInt(e.hgt)
     let hgtUnit = e.hgt.split(hgt)[1]
+
     if (byr < 1920 || byr > 2002) return false
     if (iyr < 2010 || iyr > 2020) return false
     if (eyr < 2020 || eyr > 2030) return false
@@ -29,6 +34,7 @@ let validPassports = require("fs")
     if (!e.hcl.match("^#[a-f0-9]{6}$")) return false
     if (validEyeColors.indexOf(e.ecl) == -1) return false
     if (!e.pid.match("^[0-9]{9}$")) return false
+    
     return true
   }).length
 
