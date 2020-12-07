276ae5d16be69fc6cf64fa25ac043c132dcf3658
David <david@kpstudio.co.uk>
2020-12-07 23:31:10 +0000

    needs beautifying

---
+++ 7a-solution.js
@@ -0,0 +1,27 @@
+const regex = /(\d?\s?\w+ \w+) bag/gm
+
+let rules = require("fs")
+  .readFileSync("./7-input.txt", "utf-8")
+  .split("\n")
+  .map(rule => rule.match(regex))
+  .map(rule => rule.map(bag => {
+    let bagSplit = bag.split(" ")
+    // Some have numbers at the start, some dont
+    bagSplit.length == 3 ?
+    baggie = bagSplit.splice(0,2).join(" ") : 
+    baggie = bagSplit.splice(1,2).join(" ")
+
+    return baggie
+  }))
+
+let findContainerBags = (bag) => rules.filter(e => e.slice(1).indexOf(bag) !== -1)
+
+let currentSet = ['shiny gold']
+let total = []
+
+while (currentSet.length > 0) {
+  newSet = currentSet.map(e => findContainerBags(e)).reduce((set, i) => set.concat(i), []).map(e => e[0])
+  total = total.concat(newSet)
+  currentSet = newSet 
+}
+console.log(new Set(total).size)
---
+++ 7b-solution.js
@@ -0,0 +1,28 @@
+const regex = /(\d?\s?\w+ \w+) bag/gm
+
+let rules = require("fs")
+  .readFileSync("./7-input.txt", "utf-8")
+  .split("\n")
+  .map(rule => rule.match(regex))
+  .map(rule => rule.map(bag => {
+    let bagSplit = bag.split(" ")
+    // Some have numbers at the start, some don't
+    bagSplit.length == 3 ?
+    baggie = bagSplit.splice(0,2).join(" ") : 
+    baggie = [parseInt(bagSplit[0]), bagSplit.splice(1,2).join(" ")]
+
+    return baggie
+  }))
+
+let findNumberOfBagsInside = (bag) => {
+    let rule = rules.filter(e => e[0] == bag)[0]
+    let bagsInside = rule.slice(1)
+
+    if(bagsInside[0][1] === 'no other') {
+        return 1
+    } else {
+        return bagsInside.map(bag => bag[0] * findNumberOfBagsInside(bag[1])).reduce((sum, i) => sum + i, 1)
+    }
+}
+
+console.log(findNumberOfBagsInside('shiny gold') - 1)
