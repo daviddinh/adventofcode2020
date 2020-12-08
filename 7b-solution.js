b871f53a03ba7919ca04c96391e50a922b6b1e4c
David Dinh <david@kpstudio.co.uk>
2020-12-08 11:00:15 +0000

    beautify day 7

--- 7b-solution.js
+++ 7b-solution.js
@@ -4,24 +4,28 @@ let rules = require("fs")
   .readFileSync("./7-input.txt", "utf-8")
   .split("\n")
   .map(rule => rule.match(regex))
-  .map(rule => rule.map(bag => {
-    let bagSplit = bag.split(" ")
-    bagSplit.length == 3 ?
-    baggie = bagSplit.splice(0,2).join(" ") : 
-    baggie = [parseInt(bagSplit[0]), bagSplit.splice(1,2).join(" ")]
+  .map(rule =>
+    rule.map(bag => {
+      let bagSplit = bag.split(" ")
+      bagSplit.length == 3
+        ? (baggie = bagSplit.splice(0, 2).join(" "))
+        : (baggie = [parseInt(bagSplit[0]), bagSplit.splice(1, 2).join(" ")])
 
-    return baggie
-  }))
+      return baggie
+    })
+  )
 
-let findNumberOfBagsInside = (bag) => {
+let findNumberOfBagsInside = bag => {
   let rule = rules.filter(e => e[0] == bag)[0]
   let bagsInside = rule.slice(1)
 
-  if(bagsInside[0][1] === 'no other') {
+  if (bagsInside[0][1] === "no other") {
     return 1
   } else {
-    return bagsInside.map(bag => bag[0] * findNumberOfBagsInside(bag[1])).reduce((sum, i) => sum + i, 1)
+    return bagsInside
+      .map(bag => bag[0] * findNumberOfBagsInside(bag[1]))
+      .reduce((sum, i) => sum + i, 1)
   }
 }
 
-console.log(findNumberOfBagsInside('shiny gold') - 1)
+console.log(findNumberOfBagsInside("shiny gold") - 1)
