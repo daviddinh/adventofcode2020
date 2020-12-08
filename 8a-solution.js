12b949ef6c2a1e2ffdfeaf2257364ddd5a8941d6
David Dinh <daviddinh93@gmail.com>
2020-12-08 23:00:44 +0000

    prettier

--- 8a-solution.js
+++ 8a-solution.js
@@ -1,23 +1,23 @@
 let instructions = require("fs")
   .readFileSync("./8-input.txt", "utf-8")
-  .split("\n")
+  .split("\n");
 
-let accumulator = 0
-let usedInstructions = []
-let nextInstruction = 0
-while(usedInstructions.indexOf(nextInstruction) === -1) {
-  usedInstructions.push(nextInstruction)
-  let instruction = instructions[nextInstruction].split(" ")
-  if (instruction[0] == 'acc') {
-   accumulator +=  parseInt(instruction[1])
-   nextInstruction++
+let accumulator = 0;
+let usedInstructions = [];
+let nextInstruction = 0;
+while (usedInstructions.indexOf(nextInstruction) === -1) {
+  usedInstructions.push(nextInstruction);
+  let instruction = instructions[nextInstruction].split(" ");
+  if (instruction[0] == "acc") {
+    accumulator += parseInt(instruction[1]);
+    nextInstruction++;
   }
-  if (instruction[0] == 'nop') {
-    nextInstruction++
+  if (instruction[0] == "nop") {
+    nextInstruction++;
   }
-  if (instruction[0] == 'jmp') {
-    nextInstruction += parseInt(instruction[1])
+  if (instruction[0] == "jmp") {
+    nextInstruction += parseInt(instruction[1]);
   }
 }
 
-console.log(accumulator)
+console.log(accumulator);
