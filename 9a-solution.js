f5b07f168767d287ba5ab4af3ef28eac57081630
David Dinh <david@kpstudio.co.uk>
2020-12-09 10:17:38 +0000

    day 9 complete

---
+++ 9a-solution.js
@@ -0,0 +1,19 @@
+const fs = require("fs")
+let inputText = fs.readFileSync("./9-input.txt", "utf-8")
+let input = inputText.split("\n").map(e => parseInt(e))
+
+// From Solution 1
+let findTwoElementsThatSumTo = (sum, array) => {
+  return array.filter(e => array.indexOf(sum - e) !== -1)
+}
+
+let invalidNumbers = preambleLength =>
+  input
+    .slice(preambleLength)
+    .filter(
+      (e, i) =>
+        findTwoElementsThatSumTo(e, input.slice(i, preambleLength + i)).length <
+        2
+    )
+
+console.log(invalidNumbers(25)[0])
---
+++ 9b-solution.js
@@ -0,0 +1,38 @@
+const fs = require("fs")
+let inputText = fs.readFileSync("./9-input.txt", "utf-8")
+let input = inputText.split("\n").map(e => parseInt(e))
+
+// From Solution 1
+let findTwoElementsThatSumTo = (sum, array) => {
+  return array.filter(e => array.indexOf(sum - e) !== -1)
+}
+
+let invalidNumbers = preambleLength =>
+  input
+    .slice(preambleLength)
+    .filter(
+      (e, i) =>
+        findTwoElementsThatSumTo(e, input.slice(i, preambleLength + i)).length <
+        2
+    )
+    
+let firstInvalidNumber = invalidNumbers(25)[0]
+let firstSumArray = []
+
+input.some((e, i) => {
+  let sum = 0
+  let nextNumber = i + 1
+  let sumArray = []
+
+  while (sum < firstInvalidNumber) {
+    sum += input[nextNumber]
+    sumArray.push(input[nextNumber])
+    nextNumber++
+  }
+  if (sum == firstInvalidNumber && firstSumArray.length == 0) {
+    firstSumArray = sumArray
+    return true
+  }
+})
+
+console.log(Math.min(...firstSumArray) + Math.max(...firstSumArray))
